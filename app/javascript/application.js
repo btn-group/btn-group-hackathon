3640913f3711adb1c126d6f27e9364bb90ceb608
Lucy Berwick <91242746+lucyberwick@users.noreply.github.com>
2023-05-28 11:08:22 +1000

    Replace stimulus and turbo with esbuild (#7)

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,7 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails";
-import "controllers";
+// Entry point for the build script in your package.json
+
+// jquery
+import "./src/jquery";
+
+// LISTINGS
+import "./src/listings/index";
---
+++ app/javascript/src/listings/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  if ($("#listings-index").length) {
+    LISTINGS_INDEX = {
+      init: () => {
+        console.log(123);
+      },
+    };
+
+    LISTINGS_INDEX.init();
+  }
+});
